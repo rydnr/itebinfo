9ade1d80e25d064c0230e0a844c2c86dd7c59486
chous <chous@acm-sl.org>
2015-02-08 07:03:04 +0100

    Better pagination logic.

--- search.sh
+++ search.sh
@@ -34,6 +34,7 @@ function defineErrors() {
     export TR_NOT_INSTALLED="tr is not installed";
     export QUERY_IS_MANDATORY="The search query is mandatory";
     export NO_MATCHES_FOUND="no matches found for given criteria";
+    export API_LIMIT_EXCEEDED="API limit exceeded";
 
     ERROR_MESSAGES=(\
         INVALID_OPTION \
@@ -43,6 +44,7 @@ function defineErrors() {
         TR_NOT_INSTALLED \
         QUERY_IS_MANDATORY \
         NO_MATCHES_FOUND \
+        API_LIMIT_EXCEEDED \
     );
 
     export ERROR_MESSAGES;
@@ -88,35 +90,64 @@ function checkInput() {
     fi 
 }
 
-function searchEbooks() {
+function countEbooks() {
     local _query="${1}";
     local _jsonSearch;
-    local _result;
-    local _page;
-    local _total=1;
+    local _result=0;
+    local _curlExitCode;
     createTempFile;
     _jsonSearch="${RESULT}";
 
     logInfo -n "Searching for ${_query} ebooks";
     
     curl -s "${SEARCH_URL}"/"${_query}" > "${_jsonSearch}";
+    _curlExitCode=$?;
+    
+    _result="$(cat "${_jsonSearch}" 2> /dev/null | jsawk 'return this.Error;' 2> /dev/null)";
+    if [ "x${_result}" == "x0" ]; then
+        _result="$(cat "${_jsonSearch}" | jsawk 'return this.Total;')";
+        logInfoResult SUCCESS "${_result}";
+    else
+        logInfoResult FAILURE "${_result}";
+        exitWithErrorCode API_LIMIT_EXCEEDED;
+    fi
+
+    export RESULT="${_result}";
+}
+
+function countPages() {
+    local _total=${1};
+    local _itemsPerPage=${2};
+    local _result;
+    if [ "x${_total}" == "" ]; then
+        _result=0;
+    elif [ "x${_itemsPerPage}" == "" ]; then
+        _result=$((${_total} / 10));
+    else
+       _result=$((${_total} / ${_itemsPerPage}));
+    fi
+    
+    export RESULT=${_result};
+}
+
+function searchEbooks() {
+    local _query="${1}";
+    local _page=${2};
+    local _totalPages=${3};
+    local _jsonSearch;
+    local _result;
+    createTempFile;
+    _jsonSearch="${RESULT}";
+
+    logInfo -n "Retrieving page ${_page}/${_totalPages} for '${_query}'";
+    curl -s "${SEARCH_URL}"/"${_query}/page/${_page}" > "${_jsonSearch}";
     if [ $? -eq 0 ]; then
         _result="${_result}$(cat "${_jsonSearch}" | jsawk 'return this.Books;' | jsawk 'return this.ID;' | tr -d '[' | tr -d ']' | tr -s ',' ' ')";
-        _total="$(cat "${_jsonSearch}" | jsawk 'return this.Total;')";
-        logInfoResult SUCCESS "${_total}";
+        logInfoResult SUCCESS "done";
     else
         logInfoResult FAILURE "error";
     fi
-    for _page in $(seq 1 ${_total}); do
-        logInfo -n "Retrieving page ${_page}";
-        curl -s "${SEARCH_URL}"/"${_query}/page/${_page}" > "${_jsonSearch}";
-        if [ $? -eq 0 ]; then
-            _result="${_result}$(cat "${_jsonSearch}" | jsawk 'return this.Books;' | jsawk 'return this.ID;' | tr -d '[' | tr -d ']' | tr -s ',' ' ')";
-            logInfoResult SUCCESS "done";
-        else
-            logInfoResult FAILURE "error";
-        fi
-    done
+
     export RESULT="${_result}";
 }
 
@@ -150,6 +181,9 @@ function retrieveEbookInfo() {
 
 function main() {
     local _results;
+    local _total;
+    local _totalPages;
+    local _page;
     local _url;
     local _year;
     local _isbn;
@@ -157,26 +191,34 @@ function main() {
     local _title;
     local _subtitle;
     local _filename;
-    
-    searchEbooks "${QUERY}";
-    _results="${RESULT}";
-    for _result in ${_results}; do
-        retrieveEbookInfo ${_result};
-        _url="$(echo "${RESULT}" | cut -d'#' -f 1)";
-        _year="$(echo "${RESULT}" | cut -d'#' -f 2)";
-        _isbn="$(echo "${RESULT}" | cut -d'#' -f 3)";
-        _publisher="$(echo "${RESULT}" | cut -d'#' -f 4)";
-        _title="$(echo "${RESULT}" | cut -d'#' -f 5)";
-        _subtitle="$(echo "${RESULT}" | cut -d'#' -f 6)";
-        if    [ "${_url}" != "" ] \
-           && [ "${_url}" != "undefined" ]; then
-            logInfo -n "${_publisher}-${_title}.${_subtitle}.${_year}.${_isbn}";
-            logInfoResult SUCCESS "${_url}";
-            if [ $BATCH -eq 0 ]; then
-                _filename="${_publisher}-${_title}.${_subtitle}.${_year}.${_isbn}${DEFAULT_FILE_EXTENSION}";
-                echo "[ -e \"${_filename}\" ] && echo \"${_title} already downloaded. Skipping...\"";
-                echo "[ ! -e \"${_filename}\" ] && echo \"Downloading ${_title}\" && curl -s --location --referer ${WEB_BASE_URL}${_result}/ -o \"${_filename}\" \"${_url}\"";
+
+    countEbooks "${QUERY}";
+    _total=${RESULT};
+    countPages ${_total} ${ITEMS_PER_PAGE};
+    _totalPages=${RESULT};
+
+    for _page in $(seq 1 ${_totalPages}); do
+        searchEbooks "${QUERY}" ${_page} ${_totalPages};
+        _results="${RESULT}";
+
+        for _result in ${_results}; do
+            retrieveEbookInfo ${_result};
+            _url="$(echo "${RESULT}" | cut -d'#' -f 1)";
+            _year="$(echo "${RESULT}" | cut -d'#' -f 2)";
+            _isbn="$(echo "${RESULT}" | cut -d'#' -f 3)";
+            _publisher="$(echo "${RESULT}" | cut -d'#' -f 4)";
+            _title="$(echo "${RESULT}" | cut -d'#' -f 5)";
+            _subtitle="$(echo "${RESULT}" | cut -d'#' -f 6)";
+            if    [ "${_url}" != "" ] \
+               && [ "${_url}" != "undefined" ]; then
+                logInfo -n "${_publisher}-${_title}.${_subtitle}.${_year}.${_isbn}";
+                logInfoResult SUCCESS "${_url}";
+                if [ $BATCH -eq 0 ]; then
+                    _filename="${_publisher}-${_title}.${_subtitle}.${_year}.${_isbn}${DEFAULT_FILE_EXTENSION}";
+                    echo "[ -e \"${_filename}\" ] && echo \"${_title} already downloaded. Skipping...\"";
+                    echo "[ ! -e \"${_filename}\" ] && echo \"Downloading ${_title}\" && curl -s --location --referer ${WEB_BASE_URL}${_result}/ -o \"${_filename}\" \"${_url}\"";
+                fi
             fi
-        fi
+        done
     done
 }
